09d077e12943d55a665cf3afe3b8a6d4892a250d
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2024-02-26 22:57:46 -0700

    Changed prop function to setUserResponse.

--- client/src/components/chat/Input.js
+++ client/src/components/chat/Input.js
@@ -1,10 +1,15 @@
 import React from "react";
 
-function Input() {
+function Input(props) {
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        props.setUserResponse(e.target.previousSibling.value);
+    }
+
     return (
         <form className="input">
             <input className="text" type="text" placeholder="Type a message..." />
-            <button className="sendBtn" type="submit">Send</button>
+            <button className="sendBtn" type="submit" onClick={(e) => handleSubmit(e)}>Send</button>
         </form>
     );
 }
