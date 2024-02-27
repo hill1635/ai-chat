48f1330fb4c30608a76811cf80a1bea2d86fb413
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2024-02-26 21:16:51 -0700

    Creates messages array with test messages to set up messageLog. Imported Message component and prop drilled message info into component.

--- client/src/components/chat/Log.js
+++ client/src/components/chat/Log.js
@@ -1,9 +1,17 @@
 import React from "react";
+import Message from "./Message.js";
 
 function Log() {
+    const messages = [
+        { text: "Hello", user: "user1", index: 1 },
+        { text: "Hi", user: "user2", index: 2 },
+        { text: "How are you?", user: "user1", index: 3 },
+    ];
     return(
-        <div>
-            <h1>Message log goes here</h1>
+        <div className="messageLog">
+            {messages.map((message) => {
+                return <Message key={message.index} message={message} />
+            })};
         </div>
     );
 }
