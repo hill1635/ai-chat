7c34c913b4536b4847a8d1a0318457377067cd45
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2024-02-26 22:59:31 -0700

    Deleted random import.

--- client/src/components/chat/Chat.js
+++ client/src/components/chat/Chat.js
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from "react";
 import Input from "./Input";
 import Log from "./Log";
 import "./Chat.scss";
-import { set } from "mongoose";
 
 function Chat() {
     const [ messages, setMessages ] = useState([]);
