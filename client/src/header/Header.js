2ae0c1fd56391149a21dac29b65ca3af3e3addc3
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2024-02-26 18:50:10 -0700

    Created links array with names and hrefs and prop drilled into Navigation component.

--- client/src/header/Header.js
+++ client/src/header/Header.js
@@ -3,12 +3,19 @@ import "./Header.scss";
 import Navigation from "../components/navigation/Navigation";
 import SearchBar from "../components/search/SearchBar";
 
-function Header(props) {
+function Header() {
+    const links = [
+        { name: "Home", href: "/" },
+        { name: "About", href: "/about" },
+        { name: "Login", href: "/login" },
+        { name: "Sign Up", href: "/signup" }
+    ];
+
     return (
         <header>
             <img alt="Logo" src="#"/>
             <SearchBar />
-            <Navigation links={props.routes} />
+            <Navigation links={links} />
         </header>
     );
 }
