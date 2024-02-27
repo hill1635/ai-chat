f148f259d8cf173c2d55ffbf4414deef903c2505
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2024-02-26 18:34:58 -0700

    Created routes object, changed path references, and prop drilled into Header component.

--- client/src/App.js
+++ client/src/App.js
@@ -26,16 +26,24 @@ function App() {
 //   });
 // }, []);
 
+  const routes = {
+    home: "/",
+    login: "/login",
+    signup: "/signup",
+    account: "/account",
+    about: "/about"
+  };
+
   return (
     <Router>
       <div className="background">
-        <Header loggedIn={loggedIn} />
+        <Header loggedIn={loggedIn} routes={routes} />
         <Routes>
-          <Route exact path="/" element={<Home user={user} />} />
-          <Route exact path="/login" element={<Login />} />
-          <Route exact path="/signup" element={<SignUp />} />
-          <Route exact path="/account" element={<Account />} />
-          <Route exact path="/about" element={<About />} />
+          <Route exact path={routes.home} element={<Home user={user} />} />
+          <Route exact path={routes.login} element={<Login />} />
+          <Route exact path={routes.signup} element={<SignUp />} />
+          <Route exact path={routes.account} element={<Account />} />
+          <Route exact path={routes.about} element={<About />} />
         </Routes>
         <Footer />
       </div>
