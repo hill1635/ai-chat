122b6b639a67d304c869b1f18f1427abe36aec57
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2024-03-30 14:54:10 -0600

    Cleaned up unused code.

--- client/src/pages/home/Home.js
+++ client/src/pages/home/Home.js
@@ -1,5 +1,4 @@
-import React, { useEffect, useState } from "react";
-import API from "../../utils/UserAPI";
+import React, { useEffect } from "react";
 import "./Home.scss";
 import Chat from "../../components/chat/Chat";
 
