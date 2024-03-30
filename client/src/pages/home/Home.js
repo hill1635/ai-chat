badfa5337bb43a05d5a80ab24a4f9dceab1d0abc
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2024-03-30 14:24:18 -0600

    Added chats and activeChat states in Home.js

--- client/src/pages/home/Home.js
+++ client/src/pages/home/Home.js
@@ -1,10 +1,13 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import API from "../../utils/UserAPI";
 import "./Home.scss";
 import Chat from "../../components/chat/Chat";
 import Tabs from "../../components/tabs/Tabs";
 
 function Home(props) {
+  // * chat = { id: Integer, title: String, messages: Array }
+  const [ chats, setChats ] = useState([]);
+  const [ activeChat, setActiveChat ] = useState({});
 
   useEffect(() => {
     console.log("user: ", props.user);
