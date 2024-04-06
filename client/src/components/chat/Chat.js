8f1991bb01a0546281e856d1eeeeff9437e24878
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2024-04-06 14:39:45 -0600

    Made modifications to startChat so activeChat is used as history context for chat.

--- client/src/components/chat/Chat.js
+++ client/src/components/chat/Chat.js
@@ -8,18 +8,17 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 function Chat() {
     // * chat = { id: Integer, title: String, log: Array }
     const [ chats, setChats ] = useState([]);
-    const [ activeChat, setActiveChat ] = useState({});
+    const [ activeChat, setActiveChat ] = useState([]);
 
     const genAI = new GoogleGenerativeAI(process.env.REACT_APP_GEMINI_API_KEY);
     const model = genAI.getGenerativeModel({ model: "gemini-pro" });
-    const chat = model.startChat({});
+    const chat = model.startChat({ history: activeChat });
 
     const sendMessage = async (input) => {
         const msg = input;
-        const result = await chat.sendMessage(msg);
-        const response = await result.response;
-        const text = response.text();
-        setActiveChat(chat);
+        await chat.sendMessage(msg);
+        console.log("chat:", chat);
+        setActiveChat(chat._history);
     }
 
     return(
