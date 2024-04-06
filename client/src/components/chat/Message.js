9fa3db20ceafe0120ae32866f89206978f51ccbd
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2024-04-06 14:46:41 -0600

    Adjusted props.

--- client/src/components/chat/Message.js
+++ client/src/components/chat/Message.js
@@ -3,9 +3,9 @@ import "./Chat.scss";
 
 function Message(props) {
     return(
-        <div className="messageWrapper" id={props.message.user}>
-            <span className="userName">{props.message.user}</span>
-            <span className="userMessage">{props.message.text}</span>
+        <div className="messageWrapper" id={props.message.role}>
+            <span className="userName">{props.message.role}</span>
+            <span className="userMessage">{props.message.parts[0].text}</span>
         </div>
     );
 }
