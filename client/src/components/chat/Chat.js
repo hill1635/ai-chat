5fd2731992c068096b876ad7e3e6a4ad34b51004
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2024-04-06 13:57:13 -0600

    Migrated to startChat method so chat history doesn't need to be stored and resubmitted.

--- client/src/components/chat/Chat.js
+++ client/src/components/chat/Chat.js
@@ -2,60 +2,31 @@ import React, { useEffect, useState } from "react";
 import Input from "./Input";
 import Log from "./Log";
 import Tabs from "../tabs/Tabs";
-
 import "./Chat.scss";
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
 function Chat() {
-    // * chat = { id: Integer, title: String, messages: Array }
+    // * chat = { id: Integer, title: String, log: Array }
     const [ chats, setChats ] = useState([]);
     const [ activeChat, setActiveChat ] = useState({});
-    const [ messages, setMessages ] = useState([]);
-    const [ userResponse, setUserResponse ] = useState("");
-    const [ aiResponse, setAiResponse ] = useState("");
 
     const genAI = new GoogleGenerativeAI(process.env.REACT_APP_GEMINI_API_KEY);
     const model = genAI.getGenerativeModel({ model: "gemini-pro" });
+    const chat = model.startChat({});
 
-    const convertMessagesToString = (messages) => {
-        const messageArray = [];
-        messages.forEach((message) => {
-            messageArray.push(message.text);
-        });
-        return messageArray.join(',');
-    }
-
-    const aiRun = async (input) => {
-        const messagesArray = convertMessagesToString(messages);
-        const userInput = input;
-        const prompt = "Use this as context: " + messagesArray + " Use this as the prompt:" + userInput;
-        const result = await model.generateContent(prompt);
+    const sendMessage = async (input) => {
+        const msg = input;
+        const result = await chat.sendMessage(msg);
         const response = await result.response;
         const text = response.text();
-        setAiResponse(text);
+        setActiveChat(chat);
     }
-
-    useEffect(() => {
-        if (userResponse !== "") {
-            setMessages([...messages, { text: userResponse, user: "User", index: messages.length + 1 }]);
-            aiRun(userResponse);
-        }
-    }, [userResponse]);
-
-    useEffect(() => {
-        if (aiResponse !== "") {
-            setMessages([...messages, { text: aiResponse, user: "Gemini", index: messages.length + 1 }]);
-        }
-    }, [aiResponse]);
-
-    useEffect(() => {
-    }, [messages]);
 
     return(
         <div className="chat">
             <Tabs />
-            <Log messages={messages}/>
-            <Input setUserResponse={setUserResponse}/>
+            <Log activeChat={activeChat} />
+            <Input sendMessage={sendMessage}/>
         </div>
     );
 }
