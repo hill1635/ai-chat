44f49461311f2207313a0ebfbdbb7a5d053459e2
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2024-04-07 17:16:50 -0600

    Converted activeChat to object and broke out steps to update chat log into smaller steps.

--- client/src/components/chat/Chat.js
+++ client/src/components/chat/Chat.js
@@ -9,24 +9,43 @@ function Chat() {
     // * chat = { id: Integer, title: String, log: Array }
     const [ id, setId ] = useState(0);
     const [ chats, setChats ] = useState([]);
-    const [ activeChat, setActiveChat ] = useState([]);
+    const [ activeChat, setActiveChat ] = useState({});
 
     const genAI = new GoogleGenerativeAI(process.env.REACT_APP_GEMINI_API_KEY);
     const model = genAI.getGenerativeModel({ model: "gemini-pro" });
-    const chat = model.startChat({ history: activeChat });
+    const chat = model.startChat({ history: activeChat.log });
 
+    const updateLog = (log) => {
+        setActiveChat(prevChat => ({
+            ...prevChat,
+            log: log
+        }));
+    };
+
+    const getLog = () => {
+        if (activeChat.log === undefined) {
+            return [];
+        } else {
+            return activeChat.log;
+        }
+    };
 
     const sendMessage = async (input) => {
-        setActiveChat([...activeChat, { role: "user", parts: [{ text: input }] }]);
+        var prevLog = getLog();
+        updateLog([...prevLog, { role: "user", parts: [{ text: input }] }]);
+        getResponse(input);
+    };
+
+    const getResponse = async (input) => {
         const msg = input;
         await chat.sendMessage(msg);
-        setActiveChat(chat._history);
-    }
+        updateLog(chat._history);
+    };
 
     return(
         <div className="chat">
             <Tabs />
-            <Log activeChat={activeChat} />
+            <Log chatLog={activeChat.log} />
             <Input sendMessage={sendMessage}/>
         </div>
     );
