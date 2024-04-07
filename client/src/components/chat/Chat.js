7140c3fae1ab0ee1d4434cc0e5fdd71b0e1ca1d9
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2024-04-07 17:42:33 -0600

    Deleted unused useEffect import.

--- client/src/components/chat/Chat.js
+++ client/src/components/chat/Chat.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import Input from "./Input";
 import Log from "./Log";
 import Tabs from "../tabs/Tabs";
