ea447c0c52de0736f68f6bc06ca9d846170221f0
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2024-04-07 18:47:15 -0600

    Tabs styling/formatting.

--- client/src/components/tabs/Tabs.js
+++ client/src/components/tabs/Tabs.js
@@ -1,14 +1,22 @@
 import React from "react";
+import "./Tabs.scss";
 
 function Tabs(props) {
 
+    const select = (e) => {
+        e.preventDefault();
+        console.log(e.target.getAttribute('data-id'));
+    }
+
     return(
-        <div className="tabs">
-            {props.tabs.map(tab => {
-                return <span className="tab" key={tab.id}>{tab.title}</span>
-            })}
-            <span className="tab new" onClick={props.new}>+</span>
-        </div>
+        <nav>
+            <ul className="tabs">
+                {props.tabs.map(tab => {
+                    return <li className="tab" key={tab.id} data-id={tab.id} onClick={e => select(e)}>{tab.title}</li>
+                })}
+                <li className="tab new" onClick={props.new}>+</li>
+            </ul>
+        </nav>
     );
 }
 
