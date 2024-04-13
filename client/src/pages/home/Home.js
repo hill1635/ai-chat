826cdd6019ca7d3b431b74a2254651c57e94f28a
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2024-04-13 16:07:39 -0600

    Deleted unused code/console.logs.  Prop drilled user.

--- client/src/pages/home/Home.js
+++ client/src/pages/home/Home.js
@@ -1,16 +1,12 @@
-import React, { useEffect } from "react";
+import React from "react";
 import "./Home.scss";
 import Chat from "../../components/chat/Chat";
 
 function Home(props) {
 
-  useEffect(() => {
-    console.log("user: ", props.user);
-  }, [ props.user ]);
-
   return (
     <main className="home">
-        <Chat />
+        <Chat user={props.user}/>
     </main>
   );
 }
