b208a5ca30075d052b5f43c44bc3a02cebfe0c0f
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2024-04-13 16:06:15 -0600

    Created update api function.

--- client/src/utils/UserAPI.js
+++ client/src/utils/UserAPI.js
@@ -16,4 +16,7 @@ export default {
     checkSession: function() {
         return axios.get("/api/users/login");
     },
+    update: function(id, userData) {
+        return axios.put("/api/users/" + id, userData);
+    }
 };
