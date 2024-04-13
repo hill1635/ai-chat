ed9daf7cf1438c4d74a2882dcfc0b2483224b2ce
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2024-04-13 16:13:27 -0600

    Added save function to update user.

--- client/src/components/buttons/Save.js
+++ client/src/components/buttons/Save.js
@@ -1,8 +1,16 @@
 import React from "react";
+import UserAPI from "../../utils/UserAPI";
 
 function Save(props) {
+
+    const save = () => {
+        UserAPI.update(props.user, props.data)
+            .catch((err) => {
+                console.log('err', err);
+            });
+    }
     return (
-        <button onClick={props.save}>Save</button>
+        <button onClick={save}>Save</button>
     );
 }
 
