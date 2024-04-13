05fc5b89d9c993a739ba35bd15461981f1275b5b
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2024-04-13 16:17:23 -0600

    Added Save button component to save active chat.

--- client/src/components/chat/Chat.js
+++ client/src/components/chat/Chat.js
@@ -2,10 +2,11 @@ import React, { useState, useEffect } from "react";
 import Input from "./Input";
 import Log from "./Log";
 import Tabs from "../tabs/Tabs";
+import Save from "../buttons/Save";
 import "./Chat.scss";
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
-function Chat() {
+function Chat(props) {
     const [ id, setId ] = useState(0);
     const [ chats, setChats ] = useState([]);
     const [ activeChat, setActiveChat ] = useState({});
@@ -13,6 +14,10 @@ function Chat() {
     const genAI = new GoogleGenerativeAI(process.env.REACT_APP_GEMINI_API_KEY);
     const model = genAI.getGenerativeModel({ model: "gemini-pro" });
     const chat = model.startChat({ history: activeChat.log });
+
+    var data = {
+        recents: activeChat,
+    };
 
     const initChat = () => {
         setActiveChat({
@@ -49,6 +54,7 @@ function Chat() {
     return(
         <div className="chat">
             <Tabs tabs={chats} setActive={setActiveChat} new={initChat}/>
+            <Save user={props.user} data={data}/>
             <Log chatLog={activeChat.log} />
             <Input sendMessage={sendMessage}/>
         </div>
