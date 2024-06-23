41082dff9fed8a4020399f959152a3ff4c450110
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2024-06-23 14:19:18 -0600

    Added useEffect to update message state.

--- client/src/components/chat/Message.js
+++ client/src/components/chat/Message.js
@@ -5,11 +5,18 @@ import "./Chat.scss";
 function Message(props) {
     const [ message, setMessage ] = useState({});
 
+    useEffect(() => {
+        if (props.message) {
+            setMessage(props.message);
+        }
+    }, [ props.message ]);
+
     return(
-        <div className="messageWrapper" id={props.message.role}>
-            <span className="userName">{props.message.role}</span>
-            <span className="userMessage">{props.message.parts[0].text}</span>
-        </div>
+        message &&
+            <div className="messageWrapper" id={props.message.role}>
+                <span className="userName">{props.message.role}</span>
+                <span className="userMessage">{props.message.parts[0].text}</span>
+            </div>
     );
 }
 
